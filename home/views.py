c69ffee9b34763304960a324220612ded861c7db
Crowdbotics <engineering@crowdbotics.com>
2022-02-25 08:23:37 +0000

    add package haikunator

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'haikunator', 'url': 'https://pypi.org/project/haikunator/2.1.0/'},
 	{'name':'black', 'url': 'https://pypi.org/project/black/22.1.0/'},
 	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
 	{'name':'django-bootstrap4', 'url': 'https://pypi.org/project/django-bootstrap4/0.0.7/'},
